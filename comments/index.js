4019242b889b24f4b8eb73789fdd0e1c70aa9324
omekrit <khomkrit.y@gmail.com>
2021-06-28 16:52:23 +0700

    emitting events on POST request

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 // body-parser
 app.use(express.json());
@@ -14,13 +15,26 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const postId = req.params.id;
   const { content } = req.body;
   const comments = commentsByPostId[postId] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[postId] = comments;
+  
+  // emitting CommentCreated event
+  await axios
+    .post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content,
+        postId,
+      },
+    })
+    .catch(err => console.log(err));
+
   res.status(201).send(comments);
 });
 
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 // body-parser
 app.use(express.json());
@@ -15,7 +16,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   // random post id
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
@@ -23,6 +24,17 @@ app.post('/posts', (req, res) => {
     id,
     title,
   };
+  
+  // emitting PostCreated event
+  await axios
+    .post('http://localhost:4005/events', {
+      type: 'PostCreated',
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch(err => console.log(err.message));
 
   const newPost = posts[id];
   res.status(201).send(newPost);
