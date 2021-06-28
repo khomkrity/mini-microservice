aa1d118bf99451c18cd9b722f453eee6cac686b4
omekrit <khomkrit.y@gmail.com>
2021-06-28 13:51:16 +0700

    add PostList component

---
+++ blog/client/src/PostList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  // fetch post data from post service
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+    setPosts(res.data);
+  };
+
+  // fetch post only when the page is rendered
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // array of post object
+  const renderedPosts = Object.values(posts).map(post => {
+    return (
+      <div className='card' styles={{ width: '30%', marginBottom: '20px' }} key={post.id}>
+        <div className='card-body'>
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className='d-flex flex-row flex-wrap jusity-content-between'>{renderedPosts}</div>;
+};
+
+export default PostList;
