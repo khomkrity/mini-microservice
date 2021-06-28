0d0321a22d28e1539e999dda5fe5f74b7e01ca24
omekrit <khomkrit.y@gmail.com>
2021-06-28 12:11:42 +0700

    add apis for post service

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const app = express();
+
+// body-parser
+app.use(express.json());
+
+// repository for posts
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  // random post id
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+
+  const newPost = posts[id];
+  res.status(201).send(newPost);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
