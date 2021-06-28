51765c5def89079871169d993f0323b12aca2a6c
omekrit <khomkrit.y@gmail.com>
2021-06-28 17:14:35 +0700

    parsing and storing incoming events

--- query/index.js
+++ query/index.js
@@ -5,9 +5,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
 
-app.post('/event', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  // handle evnet by type
+  // insert created post
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  // insert created comment
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
