8891d2dd36ce0f0f6205fdeefc7fcd266058178b
omekrit <khomkrit.y@gmail.com>
2021-06-29 13:58:25 +0700

    add event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,13 @@ const app = express();
 // body-parser
 app.use(express.json());
 
+// event history
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
   // post service
   axios.post('http://localhost:4000/events', event).catch(err => console.log(err.message));
   // comment service
@@ -17,6 +22,10 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event).catch(err => console.log(err.message));
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -7,12 +7,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   // handle evnet by type
   // insert created post
   if (type === 'PostCreated') {
@@ -37,10 +32,33 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
-  console.log(posts);
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  // handling events after the service is started
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      console.log('Processing event', event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
