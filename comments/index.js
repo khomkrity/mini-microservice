58e0c241a19f7499b3215642bea1b22deb63e273
omekrit <khomkrit.y@gmail.com>
2021-06-29 13:07:35 +0700

    handle commentModerated and emit CommentUpdated

--- comments/index.js
+++ comments/index.js
@@ -22,7 +22,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   const comments = commentsByPostId[postId] || [];
   comments.push({ id: commentId, content, status: 'pending' });
   commentsByPostId[postId] = comments;
-  
+
   // emitting CommentCreated event to the event bus
   await axios
     .post('http://localhost:4005/events', {
@@ -31,7 +31,7 @@ app.post('/posts/:id/comments', async (req, res) => {
         id: commentId,
         content,
         postId,
-        status: 'pending'
+        status: 'pending',
       },
     })
     .catch(err => console.log(err));
@@ -40,9 +40,28 @@ app.post('/posts/:id/comments', async (req, res) => {
 });
 
 // receive event from the event bus
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event', req.body.type);
-
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { id, status, postId, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    await axios
+      .post('http://localhost:4005/events', {
+        type: 'CommentUpdated',
+        data: {
+          id,
+          postId,
+          status,
+          content,
+        },
+      })
+      .catch(err => console.log(err));
+  }
   res.send({});
 });
 
