21f0aee00de49d8dfbc8b20f224dc52891b8357d
omekrit <khomkrit.y@gmail.com>
2021-06-29 12:59:09 +0700

    add moderation handling events

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,9 +7,14 @@ app.use(express.json());
 
 app.post('/events', (req, res) => {
   const event = req.body;
+  // post service
   axios.post('http://localhost:4000/events', event).catch(err => console.log(err.message));
+  // comment service
   axios.post('http://localhost:4001/events', event).catch(err => console.log(err.message));
+  // query service
   axios.post('http://localhost:4002/events', event).catch(err => console.log(err.message));
+  // moderation service
+  axios.post('http://localhost:4003/events', event).catch(err => console.log(err.message));
 
   res.send({ status: 'OK' });
 });
--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,26 @@ const app = express();
 // body-parser
 app.use(express.json());
 
-app.post('/events', (req, res) => {});
+// receive events from the event bus
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  // moderate comment status
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    // emit event (moderated comment) to the event bus
+    await axios
+      .post('http://localhost:4005/events', {
+        type: 'CommentModerated',
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch(err => console.log(err));
+  }
+});
 
 app.listen(4003, () => {
   console.log('Listening on 4003');
